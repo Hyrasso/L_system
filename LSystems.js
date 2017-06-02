9699d3a4679f7fd0d3c2c078925735fb1fd047e4
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2017-06-02 22:02:43 +0200

    initial commit

---
+++ LSystems.js
@@ -0,0 +1,33 @@
+'use strict';
+function LSystem(rules, start, iterations) {
+	for(var j=0;j<iterations;j++) {
+		var step = "";
+		var l = start.length;
+		for(var i=0;i<start.length;i++) {
+			var c = start.charAt(i)
+			if(c in rules) {
+				step = step + rules[c];
+			} else {
+				step += c;
+			}
+		}
+		start = step;
+	}
+	return start;
+}
+
+function interpret(string, functions, args) {
+	var stack = [];
+	args.level = 0;
+	for (var i = 0; i < string.length; i++) {
+		var c = string.charAt(i)
+		if(c in functions) {
+			functions[c](args);
+		} else if (c == "[") {
+			args.level += 1
+			stack.push(args.copy());
+		} else if (c == "]") {
+			args = stack.pop();
+		}
+	}
+}
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+var functions, args, string, trans, sc, input, cnv, start, rules, iterations;
+
+function setup() {
+	cnv = createCanvas(windowWidth, windowHeight*.8);
+	start = "AAAA";
+	input = createInput(start);
+	var btn = createButton('Reset aff');
+	btn.mousePressed(function(event){sc = 1, trans=createVector(width/2, height/2);})
+	input.input(inputcb);
+	rules = {"X" : "[F+F+F+F[---X-Y]+++++F++++++++F-F-F-F]",
+				 "A" : "X+X+X+X+X+X+",
+				 "Y" : "[F+F+F+F[---Y]+++++F++++++++F-F-F-F]"};
+	iterations = 5;
+
+	functions = {'F' : function(args) {
+					var pos = args["pos"];
+					var nextPos = p5.Vector.add(pos, p5.Vector.mult(args["dir"], args["distance"]));
+					line(pos.x, pos.y, nextPos.x, nextPos.y);
+					args["pos"] = nextPos;
+				},
+				 '+' : function(args) {
+				 	args["dir"] = args["dir"].rotate(args.angle);
+				},
+				 '-' : function(args) {
+				 	args["dir"] = args["dir"].rotate(-args.angle);
+				}};
+
+	string = LSystem(rules, start, iterations);
+	console.log(string)
+	trans = createVector(width/2, height/2);
+	sc = 1;
+	fill(255);
+	stroke(255);
+}
+
+function draw() {
+	background(51);
+	translate(trans.x, trans.y);
+	scale(sc);
+	args = {"pos" : createVector(0, 0),
+			"dir" : createVector(0, -1),
+			"distance" : 20,
+			"angle" : 15*PI/180,
+			"copy" : function() {
+				return {"pos" : this.pos.copy(),
+						"dir" : this.dir.copy(),
+						"distance" : this.distance,
+						"angle" : this.angle,
+						"copy" : this.copy}
+			}
+			};
+	interpret(string, functions, args);
+}
+
+function mouseDragged(event) {
+	if (event.srcElement.id === cnv.canvas.id) {
+		trans.add(createVector(mouseX - pmouseX, mouseY - pmouseY));
+	}
+}
+
+function mouseWheel(event) {
+	if (event.srcElement.id === cnv.canvas.id) {
+		sc += event.delta/100*sc;
+		if(sc < 0)
+			sc=0.0001
+	}
+}
+
+function keyPressed(key) {
+	if (key.key == '+' || key.char == '+') {
+		sc += .1;
+	} else if (key.key == '-' || key.char == '-') {
+		sc -= .1;
+	} else if (key.key == ' ') {
+		trans = createVector(width/2, height/2);
+		sc = 1;
+	}
+}
+
+function inputcb() {
+	start = this.elt.value
+	string = LSystem(rules, start, iterations);
+	console.log(string);
+}
